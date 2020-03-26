0296b7750e3002e9902e397977feb842dd07e122
Patrick dorgu <pat.dorgu@gmail.com>
2020-03-26 18:40:53 -0400

    Delete seeds.sql

--- db/seeds.sql
+++
@@ -1,7 +0,0 @@
-USE burger_db;
-
-INSERT INTO burgers (burger_name) VALUES ('cheeseBurger');
-
-INSERT INTO burgers (burger_name) VALUES ('hamburgerBurger');
-
-INSERT INTO burgers (burger_name) VALUES ('impossible Burger');
